22cdd10d4cad3423d252d573fce51228e2d2c4c9
hannahschwen <hannahschwen@gmail.com>
2024-07-16 17:03:50 -0400

    updated env

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize (
-  "todo_app_db",
-  "postgres",
-  "hannarue",
+const sequelize = new Sequelize(process.env.PIKACHU_DB, process.env.PIKACHU_USER,
+  process.env.PIKACHU_PW,
+
   {
     host: 'localhost',
     dialect: 'postgres'
